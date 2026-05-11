e72bd92e414d1a6aea92a0c77477ae07402c6fd7
Kin-Wang Yau <kyau0441@uni.sydney.edu.au>
2026-05-11 10:49:11 +1000

    expanded model (garch ones still in progress

--- back_end/utils/models.py
+++ back_end/utils/models.py
@@ -1,10 +1,18 @@
 from sklearn.ensemble import RandomForestRegressor
 from xgboost import XGBRegressor
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression, LassoCV, RidgeCV
+from sklearn.tree import DecisionTreeRegressor
 from lightgbm import LGBMRegressor
-
+from arch import arch_model
 
 def make_models():
     return {
+        "MLR": LinearRegression(),
+        "Lasso": LassoCV(cv=5, random_state=42),
+        "Ridge": RidgeCV(cv=5),
+        "DT": DecisionTreeRegressor(max_depth=6, random_state=42),
         "RF": RandomForestRegressor(
             n_estimators=200, random_state=42, n_jobs=-1
         ),
